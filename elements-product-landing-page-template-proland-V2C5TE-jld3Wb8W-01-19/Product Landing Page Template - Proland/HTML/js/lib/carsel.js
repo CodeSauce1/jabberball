9d0e02c87fbbac45ce4598d0d90ff6896e7324ce
jaber khan <jaberalikhan7@gmail.com>
2020-11-23 17:06:28 -0600

    new setup

---
+++ elements-product-landing-page-template-proland-V2C5TE-jld3Wb8W-01-19/Product Landing Page Template - Proland/HTML/js/lib/carsel.js
@@ -0,0 +1,51 @@
+// JavaScript source code
+
+let selectElement = document.querySelector('.selectpicker');
+
+let Tiger=document.getElementById('tiger')
+let Rabbit=document.getElementById('rabbit')
+let Pig=document.getElementById('pig')
+let Dog=document.getElementById('dog')
+let Basket=document.getElementById('basket')
+
+selectElement.addEventListener('change', (event) => {
+if(selectElement.value=="Tiger"){
+Tiger.className="item active";
+Rabbit.className="item";
+Pig.className="item";
+Dog.className="item";
+Basket.className="item";
+
+}else{
+	Tiger.className="item"
+}
+if(selectElement.value=="Dog"){
+Tiger.className="item";
+Rabbit.className="item";
+Pig.className="item";
+Dog.className="item active";
+Basket.className="item";
+}else{
+	Dog.className="item"
+}
+if(selectElement.value=="Rabbit"){
+
+Tiger.className="item";
+Rabbit.className="item active";
+Pig.className="item";
+Dog.className="item";
+Basket.className="item";
+}else{
+	Rabbit.className="item"
+}
+if(selectElement.value=="Pig"){
+Tiger.className="item";
+Rabbit.className="item";
+Pig.className="item active";
+Dog.className="item";
+Basket.className="item";
+}else{
+	Pig.className="item"
+}
+
+});
