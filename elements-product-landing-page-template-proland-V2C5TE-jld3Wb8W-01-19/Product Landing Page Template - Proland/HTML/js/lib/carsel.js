38e7f33c5811f01599d45d24125d1f3bea9dc9da
jaber khan <jaberalikhan7@gmail.com>
2020-11-30 02:53:18 -0600

    new products

--- elements-product-landing-page-template-proland-V2C5TE-jld3Wb8W-01-19/Product Landing Page Template - Proland/HTML/js/lib/carsel.js
+++ elements-product-landing-page-template-proland-V2C5TE-jld3Wb8W-01-19/Product Landing Page Template - Proland/HTML/js/lib/carsel.js
@@ -1,11 +1,16 @@
 // JavaScript source code
 
 let selectElement = document.querySelector('.selectpicker');
-
+let price=document.querySelector('.pain')
+let more=document.getElementById('more')
 let Tiger=document.getElementById('tiger')
 let Rabbit=document.getElementById('rabbit')
 let Pig=document.getElementById('pig')
 let Dog=document.getElementById('dog')
+let pinkpig=document.getElementById('pinkpig')
+let minipig=document.getElementById('minipig')
+let pug=document.getElementById('pug')
+let bear=document.getElementById('bear')
 let Basket=document.getElementById('basket')
 let element = document.getElementById("product-imgs");
 
@@ -15,21 +20,29 @@ Tiger.className="item active";
 Rabbit.className="item";
 Pig.className="item";
 Dog.className="item";
+pug.className="item"
+pinkpig.className="item"
+minipig.className="item"
+bear.className="item"
 Basket.className="item";
 element.scrollIntoView();
 
 }else{
-	Tiger.className="item"
+	Tiger.className="item";
 }
 if(selectElement.value=="Dog"){
 Tiger.className="item";
 Rabbit.className="item";
 Pig.className="item";
 Dog.className="item active";
+pug.className="item"
+pinkpig.className="item"
+minipig.className="item"
+bear.className="item"
 Basket.className="item";
 element.scrollIntoView();
 }else{
-	Dog.className="item"
+	Dog.className="item";
 }
 if(selectElement.value=="Rabbit"){
 
@@ -37,20 +50,92 @@ Tiger.className="item";
 Rabbit.className="item active";
 Pig.className="item";
 Dog.className="item";
+pug.className="item"
+pinkpig.className="item"
+minipig.className="item"
+bear.className="item"
 Basket.className="item";
 element.scrollIntoView();
 }else{
-	Rabbit.className="item"
+	Rabbit.className="item";
 }
-if(selectElement.value=="Pig"){
+if(selectElement.value=="Purple Pig"){
 Tiger.className="item";
 Rabbit.className="item";
 Pig.className="item active";
 Dog.className="item";
+pug.className="item";
+pinkpig.className="item";
+minipig.className="item";
+bear.className="item";
 Basket.className="item";
 element.scrollIntoView();
 }else{
-	Pig.className="item"
+	Pig.className="item";
 }
+
+if(selectElement.value=="Pink Pig"){
+Tiger.className="item";
+Rabbit.className="item";
+Pig.className="item";
+Dog.className="item";
+pug.className="item";
+pinkpig.className="item active";
+minipig.className="item";
+bear.className="item";
+Basket.className="item";
+element.scrollIntoView();
+}else{
+	pinkpig.className="item";
+}
+
+if(selectElement.value=="Bear"){
+Tiger.className="item";
+Rabbit.className="item";
+Pig.className="item";
+Dog.className="item";
+pug.className="item";
+pinkpig.className="item";
+minipig.className="item";
+bear.className="item active";
+Basket.className="item";
+element.scrollIntoView();
+}else{
+	bear.className="item";
+}
+
+if(selectElement.value=="Mini Pig"){
+Tiger.className="item";
+Rabbit.className="item";
+Pig.className="item";
+Dog.className="item";
+pug.className="item"
+pinkpig.className="item";
+minipig.className="item active";
+bear.className="item";
+Basket.className="item";
+price.innerText="10.00";
+element.scrollIntoView();
+}else{
+	minipig.className="item";
+	price.innerText="14.99";
+}
+if(selectElement.value=="Pug"){
+Tiger.className="item";
+Rabbit.className="item";
+Pig.className="item";
+Dog.className="item";
+pug.className="item active"
+pinkpig.className="item";
+minipig.className="item";
+bear.className="item";
+Basket.className="item";
+price.innerText="15.99";
+element.scrollIntoView();
+}else{
+	pug.className="item";
+	price.innerText="14.99";
+}
+
 
 });
