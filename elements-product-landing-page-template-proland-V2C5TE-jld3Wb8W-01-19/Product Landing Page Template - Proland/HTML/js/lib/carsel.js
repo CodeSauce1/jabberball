6e6a4477221fd1bd5568dbb0241593a0e24a24c8
jaber khan <jaberalikhan7@gmail.com>
2020-11-29 18:15:55 -0600

    scroll fix

--- elements-product-landing-page-template-proland-V2C5TE-jld3Wb8W-01-19/Product Landing Page Template - Proland/HTML/js/lib/carsel.js
+++ elements-product-landing-page-template-proland-V2C5TE-jld3Wb8W-01-19/Product Landing Page Template - Proland/HTML/js/lib/carsel.js
@@ -7,6 +7,7 @@ let Rabbit=document.getElementById('rabbit')
 let Pig=document.getElementById('pig')
 let Dog=document.getElementById('dog')
 let Basket=document.getElementById('basket')
+let element = document.getElementById("product-imgs");
 
 selectElement.addEventListener('change', (event) => {
 if(selectElement.value=="Tiger"){
@@ -15,7 +16,7 @@ Rabbit.className="item";
 Pig.className="item";
 Dog.className="item";
 Basket.className="item";
-window.scrollTo(0,0);
+element.scrollIntoView();
 
 }else{
 	Tiger.className="item"
@@ -26,7 +27,7 @@ Rabbit.className="item";
 Pig.className="item";
 Dog.className="item active";
 Basket.className="item";
-window.scrollTo(0,0);
+element.scrollIntoView();
 }else{
 	Dog.className="item"
 }
@@ -37,7 +38,7 @@ Rabbit.className="item active";
 Pig.className="item";
 Dog.className="item";
 Basket.className="item";
-window.scrollTo(0,0);
+element.scrollIntoView();
 }else{
 	Rabbit.className="item"
 }
@@ -47,7 +48,7 @@ Rabbit.className="item";
 Pig.className="item active";
 Dog.className="item";
 Basket.className="item";
-window.scrollTo(0,0);
+element.scrollIntoView();
 }else{
 	Pig.className="item"
 }
